9afbe9dd3f4e8a0394742ae18df950a297d3b3b9
mahmudkhalifa <mahmoudkh@silverkeytech.com>
2020-06-07 06:59:53 +0200

    Add Infrastructure project

---
+++ Infrastructure/Data/RepositoryImplementation/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.RepositoryImplementation
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _context;
+
+        public ProductRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _context.Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType)
+                .ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products
+                .Include(p => p.ProductBrand)
+                .Include(p => p.ProductType)
+                .FirstOrDefaultAsync(p=>p.Id == id);
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetBrandsAsync()
+        {
+            return await _context.ProductBrands.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
+        }
+    }
+}
