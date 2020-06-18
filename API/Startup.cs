2e6939023646248233194967c2cc47270f7b54f9
khalifa005 <mahmudkhalifa1@gmail.com>
2020-06-18 21:13:12 +0200

    update startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,8 @@ namespace API
 
             services.AddDbContext<StoreContext>(options =>
                 options.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
