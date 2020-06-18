1f74a3efab4b0553b40cae7d72f8b9f17f1026b1
khalifa005 <mahmudkhalifa1@gmail.com>
2020-06-18 22:30:55 +0200

    Add implementatio for generic repository

--- Infrastructure/Data/RepositoryImplementation/GenericRepository.cs
+++ Infrastructure/Data/RepositoryImplementation/GenericRepository.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Core.Entities;
 using Core.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data.RepositoryImplementation
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
-        public System.Threading.Tasks.Task<IReadOnlyList<T>> GetAllAsync()
+        private readonly StoreContext _context;
+
+        public GenericRepository(StoreContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
         }
 
-        public System.Threading.Tasks.Task<T> GetByIdAsync(int id)
+        public async Task<T> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Set<T>().FindAsync(id);
         }
     }
 }
