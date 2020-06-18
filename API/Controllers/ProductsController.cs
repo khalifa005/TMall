e29850e113002e7e9b62c2353fe2ca88fc063261
khalifa005 <mahmudkhalifa1@gmail.com>
2020-06-18 22:31:17 +0200

    Update product controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,17 +10,24 @@ namespace API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductRepository _productRepository;
+        private readonly IGenericRepository<Product> _productRepository;
+        private readonly IGenericRepository<ProductType> _productTypeRepository;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepository;
 
-        public ProductsController(IProductRepository productRepository)
+        public ProductsController(
+            IGenericRepository<Product> productRepository, 
+            IGenericRepository<ProductType> productTypeRepository,
+            IGenericRepository<ProductBrand> productBrandRepository)
         {
             _productRepository = productRepository;
+            _productTypeRepository = productTypeRepository;
+            _productBrandRepository = productBrandRepository;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _productRepository.GetProductsAsync();
+            var products = await _productRepository.GetAllAsync();
 
             return Ok(products);
         }
@@ -28,40 +35,40 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProductById(int id)
         {
-            return await _productRepository.GetProductByIdAsync(id);
+            return await _productRepository.GetByIdAsync(id);
         }
 
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetBrands()
         {
-            return Ok(await _productRepository.GetBrandsAsync());
+            return Ok(await _productBrandRepository.GetAllAsync());
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetTypes()
         {
-            return Ok(await _productRepository.GetTypesAsync());
+            return Ok(await _productTypeRepository.GetAllAsync());
         }
 
-        [HttpPost("add")]
-        public async Task<ActionResult<Product>> AddProduct(Product product)
-        {
-            var result= await _productRepository.CreateProductAsync(product);
-            return Ok(result);
-        }
+        //[HttpPost("add")]
+        //public async Task<ActionResult<Product>> AddProduct(Product product)
+        //{
+        //    var result= await _productRepository.CreateProductAsync(product);
+        //    return Ok(result);
+        //}
 
-        [HttpPut("edit")]
-        public async Task<ActionResult<Product>> UpdateProduct(Product product)
-        {
-            var result= await _productRepository.UpdateProductByIdAsync(product);
-            return Ok(result);
-        }
+        //[HttpPut("edit")]
+        //public async Task<ActionResult<Product>> UpdateProduct(Product product)
+        //{
+        //    var result= await _productRepository.UpdateProductByIdAsync(product);
+        //    return Ok(result);
+        //}
 
-        [HttpDelete("delete")]
-        public ActionResult DeleteProduct(int id)
-        {
-            _productRepository.DeleteProductByIdAsync(id);
-            return Ok();
-        }
+        //[HttpDelete("delete")]
+        //public ActionResult DeleteProduct(int id)
+        //{
+        //    _productRepository.DeleteProductByIdAsync(id);
+        //    return Ok();
+        //}
     }
 } 
